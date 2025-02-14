b11c8a62e858da64295d57059e224264f1af2917
Magdalena Yankova <maggie.yankovaaa@gmail.com>
2025-02-14 16:30:40 +0100

    Update .mocharc.js

--- .mocharc.js
+++ .mocharc.js
@@ -1,7 +1,7 @@
 // .mocharc.js - Mocha config file
 const {colors, symbols} = 
   require('mocha/lib/reporters/base');
-// Change the default console UI colors edit
+// Change the default console UI colors 
 colors.pass = 32;
 colors.fail = 33;
 colors["error stack"] = 36;
